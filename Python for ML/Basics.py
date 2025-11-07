ff1d544597265c45e48ab8feab0c1a8f1bd9cee0
vinaysukase <vinaysukasework@gmail.com>
2025-11-07 13:19:22 +0530

    basics done

---
+++ Python for ML/Basics.py
@@ -0,0 +1,32 @@
+# Variables
+
+# StudentName = "vinay sukase" :-  Pascal Case
+# studentName = "vinay sukase" :-  Camel Case
+# student_mame = "vinay sukase" :-  Snake Case
+
+# Type
+
+# a = -34
+# b = 56.8
+# c = 12 / 3
+# type(a)
+# type(b)
+# type(c)
+
+# String Slicing
+# subname = "machinelearning"
+# print(subname[::]) :- prints entire string
+# print(subname[4:]) :- output ( inelearning )
+# print(subname[:4]) :- output ( mach )
+
+# taking input & output
+# print("Welcome to the Rollercoaster Ride")
+# name = input("Please enter your name: ")
+# age = input("Please enter your age: ")
+# print("Hello ",name, " welcome to the rollercoaster.","\n Your age is ",age," \n You can ride the machine.")
+# print("Enjoy your ride!")
+
+# f-String
+# city = input("Enter City: ")
+# famous_location = input("Enter famous location: ")
+# print(f"{city} is well known because of {famous_location}")
