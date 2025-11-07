2370c6849c79ebdb189d0a8f44b1e25a388fb4cd
vinaysukase <vinaysukasework@gmail.com>
2025-11-07 19:02:08 +0530

    operators.py add

---
+++ Python for ML/operators.py
@@ -0,0 +1,98 @@
+# OPERATORS :- operators are symbols that perform operations on variables and values.
+# Python has several operators like arithmatic, comparison, logical operators and more
+
+# ------------------------------------------------------------------------------------------------- #
+
+# ARITHMETIC OPERATORS :- arithmetic operators perform mathematical operations
+a = 15
+b = 5
+
+# ADDITION
+add = a + b
+print(add)
+
+# SUBSTRACTION
+sub = a - b
+print(sub)
+
+# MULTIPLICATION
+mul = a * b
+print(mul)
+
+# DIVISION
+div = a / b
+print(div)
+
+# FLOOR DIVISION
+fdiv = a // b
+print(fdiv)
+
+# MODULUS
+mod = a % b
+print(mod)
+
+# EXPONENTIAL
+pow = a ** b
+print(pow)
+
+# ------------------------------------------------------------------------------------------------- #
+
+#  ASSIGNMENT OPERATORS :-  Assignment operators are used to assign values to variables
+
+# Basic assignment operator is =
+a = 20
+
+# COMPOUND ASSIGNMENT OPERATOR :- it combines arithmetic operations with assignment
+
+# ADD and ASSIGN
+a += 20
+print(a)
+
+# SUB and ASSIGN
+a -= 20
+print(a)
+
+# MUL and ASSIGN
+a *= 20
+print(a)
+
+# Div and ASSIGN
+a /= 20
+print(a)
+
+# FloorDiv and ASSIGN
+a //= 20
+print(a)
+
+# Mod and ASSIGN
+a %= 20
+print(a)
+
+# Expo and ASSIGN
+a **= 20
+print(a)
+
+# ------------------------------------------------------------------------------------------------- #
+
+# COMPARISON OPERATOR :- are also called as relational operators, used to compare two values
+# comparison operators provide Boolean result that is True and False
+a = 15
+b = 15
+
+# equal to
+print(a == b)
+
+# not equal to
+print(a != b)
+
+# greater than
+print(a > b)
+
+# less than
+print(a < b)
+
+# greater than or equal to
+print(a >= b)
+
+# less than or equal to
+print(a <= b)
