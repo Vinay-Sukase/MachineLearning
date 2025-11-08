d133ab711627e77e893737e40ca80fcfac7541d3
Vinay-Sukase <vinaysukasework@gmail.com>
2025-11-08 20:22:38 +0530

    loops.py added

---
+++ Python for ML/Loops_questions.py
@@ -0,0 +1,13 @@
+# Q1. Accept an integer and Print hello world n times
+# Q2. Print natural number up to n
+# Q3. Reverse for loop. Print n to 1
+# Q4. Take a number as input and print its table
+# Q5. Sum up to n terms
+# Q6. Factorial of a number
+# Q7. Print the sum of all even & odd numbers in a range separately
+# Q8. Print all the factors of a number
+# Q9. Accept a number and check if it's a perfect number or not
+# Q10. Reverse a string without using in build functions
+# Q11. Check string is Palindrome or not
+# Q12.  Count all letters, digits, and special symbols from a given string
+str1 = "P@#yn26at^&i5ve"
